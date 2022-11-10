06ddbc4a2ebc10b89fea41a8c05bf3d67d6df234
njfernandez <108493015+njfernandez@users.noreply.github.com>
2022-11-10 18:37:03 -0300

    Delete DESAFIO FINAL!! directory

--- DESAFIO FINAL!!/js/menu.js
+++
@@ -1,3 +0,0 @@
-const toggleMenuElement= document.getElementById('toggle-menu');
-const mainMenuElemnt= document.getElementById('main-menu');
-toggleMenuElement.addEventListener('click', ()=>{mainMenuElemnt.classList.toggle('main-menu--show')});
